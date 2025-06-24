9e73668d65dec12c2c04d55d194490ee2a37c5f8
Siddharth <siddharthgautamvns@gmail.com>
2025-06-25 03:46:41 +0530

    second draft

--- include/constants.h
+++ include/constants.h
@@ -1,3 +1,5 @@
 #pragma once
 
+// can change to more or less.
 const int THREAD_COUNT = 4;
+ 
--- include/env_loader.h
+++ include/env_loader.h
@@ -1,2 +1,4 @@
 #pragma once
+
+//just to be professional , i used .env directory to store my XOR_KEY
 char loadKeyFromEnv();
